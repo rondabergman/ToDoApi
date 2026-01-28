9ab5ad95edc8b488d1266d884ec20f0e436bd687
ronda <rondabergman@hotmail.com>
2026-01-27 19:10:32 -0500

    Added ToDo Object Added Tests for ToDo

---
+++ ToDoApi.Tests/ToDoTests.cs
@@ -0,0 +1,35 @@
+﻿namespace ToDoApi.Tests
+{
+    public class ToDoTests
+    {
+        [Fact]
+        public void CanCreate_NewToDo_With_Title_CreateDate_Description_DueDate()
+        {
+            var toDo = new ToDo
+            {
+                Title = "Sample Title",
+                Description = "Description",
+                DueDate = new DateOnly(2026, 12, 1)
+            };
+
+            Assert.NotNull(toDo);
+            Assert.Equal(new DateOnly(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day), toDo.CreateDate);
+            Assert.Equal("Description", toDo.Description);
+            Assert.Equal(new DateOnly(2026, 12, 1), toDo.DueDate);
+        }
+
+        [Fact]
+        public void CanCreate_NewToDo_With_Only_A_Title()
+        {
+            var toDo = new ToDo
+            {
+                Title = "Sample Title"
+            };
+
+            Assert.NotNull(toDo);
+            Assert.Equal(new DateOnly(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day), toDo.CreateDate);
+            Assert.Null(toDo.Description);
+            Assert.Null(toDo.DueDate);
+        }
+    }
+}
---
+++ ToDoApi/ToDo.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoApi
+{
+    public class ToDo
+    {
+        public DateOnly CreateDate { get; private set; } = DateOnly.FromDateTime(DateTime.Now);
+
+        public required string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateOnly? DueDate { get; set; }
+    }
+}
