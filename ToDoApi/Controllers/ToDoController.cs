bb64b811459f233a0b245a3e06d8a8f57852e615
ronda <rondabergman@hotmail.com>
2026-01-27 20:17:31 -0500

    Deleted: weatherforcast controller Added: temp data to todo get controller

---
+++ ToDoApi/Controllers/ToDoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ToDoApi.Controllers
+{
+    [ApiController]
+    [Route("api/todos")]
+    public class ToDoController : ControllerBase
+    {
+        private readonly List<ToDo> _tempToDos = new List<ToDo>
+        {
+            new ToDo { Title = "title1" },
+            new ToDo { Title = "title2" },
+            new ToDo { Title = "title3" }
+        };
+
+        [HttpGet(Name = "GetToDos")]
+        public ActionResult<IEnumerable<ToDo>> Get()
+        {
+            return Ok(_tempToDos);
+        }
+    }
+}
